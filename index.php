8da3b492f93db6711d7c684e444a6661be5dd84f
Abhimat Gautam <abhimat.gautam@gmail.com>
2016-04-09 00:37:36 -0700

    Favicon and Touch Icon

--- index.php
+++ index.php
@@ -54,8 +54,8 @@ if ((!isset($_POST["submit"])) and (!isset($_POST["submitcheck"])))
 	<![endif]-->
 	
 	<!-- Favicon -->
-	<link rel="shortcut icon" href="./favicon.png">
-	<link rel="apple-touch-icon" href="./touch-icon.png">
+	<link rel="shortcut icon" href="images/favicon.png">
+	<link rel="apple-touch-icon" href="images/touch-icon.png">
 	
 	<!-- Styles -->
 	<link rel="stylesheet" href="./styles.css" type="text/css" media="screen" />
@@ -103,7 +103,7 @@ if ((!isset($_POST["submit"])) and (!isset($_POST["submitcheck"])))
 	</div> <!-- /content -->
 	
 	<footer class="small wrap">
-		<center><p class="small"><strong>Astro Coffee 2</strong><br>by Abhimat Gautam</p></center>
+		<center><p class="small"><strong>Astro Coffee 2</strong><br>by <a href="http://astro.ucla.edu/~abhimat/">Abhimat Gautam</a></p></center>
 		<center><p class="small">Based on previous versions of astroph.py by<br>Ryan T. Hamilton, Ian J. Crossfield, and Nathaniel Ross.</p></center>
 		<center><p class="small"><a href="https://github.com/abhimat/AstroCoffee">Contribute on GitHub!</a></p></center>
 	</footer>
