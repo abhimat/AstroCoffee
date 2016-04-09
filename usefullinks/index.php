5b6d49a51f54a042339136150da99512ad6ef324
Abhimat Gautam <abhimat.gautam@gmail.com>
2016-04-09 16:43:55 -0700

    Made header image link to main page.

--- usefullinks/index.php
+++ usefullinks/index.php
@@ -44,7 +44,7 @@
 	<header>
 		<!-- <p><a href="http://www.astro.ucla.edu"><semibold><em>UCLA</em> Astronomy and Astrophysics</semibold></a></p> -->
 		<!-- <h1>Astro Coffee</h1> -->
-		<img src="../images/Header_2x.png" srcset="../images/Header_1x.png 1x, ../images/Header_2x.png 2x" alt="Astro Coffee" />
+		<a href="../"><img src="../images/Header_2x.png" srcset="../images/Header_1x.png 1x, ../images/Header_2x.png 2x" alt="Astro Coffee" /></a>
 		
 		<center><ul class="pages">
 			<li><a href="../">Current Articles</a></li>
