fb05c201b84ed4d8c1df8b65f1d5af3b586e6e95
Abhimat Gautam <abhimat.gautam@gmail.com>
2016-04-12 22:43:00 -0700

    Better error handling on the web parser

--- getwebinfo.py
+++ getwebinfo.py
@@ -9,20 +9,26 @@ def getwebinfo(url, html):
     from bs4 import BeautifulSoup
     import re
     from astroph import preprint
+    import datetime
     
     import json
     import requests
     
     paper = preprint()
     
+    paper.errors = "0"
+    
     # Use Instaparser to parse the article
     ip_api_key = 'b4a72359c1ba49569df55a9ccfcc1390'
     
     ip_params = {'api_key': ip_api_key, 'url': url}
     
-    ip_response = requests.get('https://www.instaparser.com/api/1/article', params=ip_params)
-    
-    ip_data = json.loads(ip_response.text)
+    try:
+        ip_response = requests.get('https://www.instaparser.com/api/1/article', params=ip_params)
+        ip_data = json.loads(ip_response.text)
+    except:
+        paper.errors = "1"
+        print("Error grabbing webpage from Instaparser")
     
     # print(ip_response.url)
     # print(ip_response.status_code)
@@ -32,15 +38,41 @@ def getwebinfo(url, html):
     # print(ip_data['description'].encode("utf-8"))
 
     paper.url = url
-    paper.title = ip_data['title'].encode("utf-8")
-    paper.errors = "0"
-    paper.numauth = 1
-    paper.author = ip_data['author'].encode("utf-8")
-    paper.abstract = ip_data['description'].encode("utf-8")
-    paper.date = 'Web Article:'
-    paper.sources = ''
+    
+    paper.sources = 'Web Article'
     paper.subject = ''
     paper.comments = ''
+    
+    try:
+        paper.title = ip_data['title'].encode("utf-8")
+    except:
+        paper.errors = "0"
+        paper.title = "Error Grabbing Title"
+        # print("Error grabbing title")
+    
+    try:
+        date = ip_data['date']
+        # print(date)
+        paper.date = datetime.datetime.fromtimestamp(int(date)).strftime('%d %b %Y')
+    except:
+        paper.errors = "0"
+        paper.date = " "
+        # print("Error grabbing Date")
+    
+    try:
+        paper.numauth = 1
+        paper.author = ip_data['author'].encode("utf-8")
+    except:
+        paper.errors = "0"
+        paper.author = "Error Grabbing Authors"
+        # print("Error grabbing authors")
+    
+    try:
+        paper.abstract = ip_data['description'].encode("utf-8")
+    except:
+        paper.errors = "0"
+        paper.abstract = "Error Grabbing Description"
+        # print("Error grabbing description")
     
     # paper.url = url
     # # Remove all the muck that screws up the BeautifulSoup parser
@@ -76,5 +108,4 @@ def getwebinfo(url, html):
     # paper.subject = ''
     # paper.comments = ''
     
-    print(paper)
     return paper
