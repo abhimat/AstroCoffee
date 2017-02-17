876c0c3c85827bfe52aebb88333d3ba3ccff283c
Abhimat Gautam <abhimat.gautam@gmail.com>
2017-02-17 11:39:54 -0800

    Complete PHP script to add name to papers_volunteers list

--- volunteer/volunteer.php
+++ volunteer/volunteer.php
@@ -18,94 +18,75 @@
 	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 	
 	<!-- Styles -->
-	<link rel="stylesheet" href="./styles.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="../styles.css" type="text/css" media="screen" />
 </head>
 
 <body>
+    <h2>Volunteer to discuss a paper</h2>
 
+    <?php
+        //
+        //
+        // If you see this text in your browser, it means PHP scripts are not
+        // enabled on this web server.  Consult the internet or your local system
+        // administrator to enable PHP scripting.
+        //
+        //
 
-<?php
+        // SET THE PATH TO YOUR PYTHON VERSION HERE
+        // $python = '/usr/bin/python';
 
-//
-//
-// If you see this text in your browser, it means PHP scripts are not
-// enabled on this web server.  Consult the internet or your local system
-// administrator to enable PHP scripting.
-//
-//
+        $ID = $_GET['ID'];
 
-// SET THE PATH TO YOUR PYTHON VERSION HERE
-// $python = '/usr/bin/python';
+        $python = '/Users/abhimat/software/Ureka/variants/common/bin/python';
 
-$ID = $_GET['ID'];
+        // CHOOSE YOUR TIMEZONE FROM THE LINK BELOW AND SET IT HERE
+        $CurrentVer = phpversion();
+        if(version_compare($CurrentVer, '5.1.0')>=0) {
+            date_default_timezone_set('US/Pacific');
+            #http://www.php.net/manual/en/timezones.others.php
+        }
 
-$python = '/Users/abhimat/software/Ureka/variants/common/bin/python';
+        $paperFile_volunteer = '../papers_volunteers';
 
-// CHOOSE YOUR TIMEZONE FROM THE LINK BELOW AND SET IT HERE
-$CurrentVer = phpversion();
-if(version_compare($CurrentVer, '5.1.0')>=0) {
-    date_default_timezone_set('US/Pacific');
-    #http://www.php.net/manual/en/timezones.others.php
-}
+        $logFile = "../dregs.log";
+        $listManager = "../listmanager.php";
+        // $ID = $_POST["article"];
+        $ipOfSubmitter=$_SERVER["REMOTE_ADDR"];
 
-$paperFile = "../papers_discussed";
-$logFile = "../dregs.log";
-$listManager = "../listmanager.php";
-// $ID = $_POST["article"];
-$ipOfSubmitter=$_SERVER["REMOTE_ADDR"];
+        $minlength = 2;
+        echo "<p>Paper to volunteer for: ".$ID.".</p>";
+    ?>
 
-$minlength = 5;
-// echo '<META HTTP-EQUIV=Refresh CONTENT="10">';
-echo '<META HTTP-EQUIV=Refresh CONTENT="1;url=../">';
-echo "<p>You submitted ".$ID.".</p>";
+    <form name="form" method="post" action="<?php echo $_SERVER['php_SELF'];?>">
+        Your name: <input type="text" name="volunteer_name" />
+        <input type="submit" name="save_volunteer" value="Submit">
+    </form>
 
-# Testing for coffee time (can't submit otherwise (silly robots :( ))
-if (!((date('H') >= 11 and date('H') < 12) and (date('w') <= 5 and date('w') >= 1))) {
-	echo '<p><strong>Not coffee time!</strong></p>';
-	echo '<p>Paper not added to discussed list. Please wait until coffee time to add.</p>';
-    echo "<p>Returning to the main page automatically in 1 second</p>";
-    echo "<p>If not, click <a href='../'>here</a></p>";
-	die;
-}
-
-# Testing for duplicates
-$papers = file($paperFile) or die("can't open file: ".$paperFile);
-foreach ($papers as $pap){
-	if (trim($ID) == trim($pap)) {
-		echo '<p>But it is a duplicate of a paper already submitted for the discussed list!</p>';
-		echo '<p>If you think this is an error, tell the coffee czar.</p>';
-        echo "<p>Returning to the main page automatically in 1 second</p>";
-        echo "<p>If not, click <a href='../'>here</a></p>";
-		die;
-	}
-}
-
-
-# Valid submission
-if (strlen($ID)>$minlength) {
-	$f = fopen($paperFile,"a") or die("can't open file: ".$paperFile);
-	fwrite($f, "\n".trim($ID));
-	fclose($f);
-	# Get estimate of run time for the user
-	$lines = count(file($paperFile)); 
-	$time  = $lines * 10;
-	echo "<p>The paper was successfully added to the discussed papers list.</p>";
-	echo "<p>It may be several minutes before the main page is updated.</p>";
-	echo "<p>You will be returned to the main page in 1 second.</p>";
-	echo "<p>If not, click <a href='../'>here</a></p>";
-	$str4log = date('D, d M Y H:i:s')."    ".$ID."    ".$ipOfSubmitter."    Paper added to discussed papers list.";
-	$f2 = fopen($logFile,"a") or die("can`t open file: ".$logFile);
-	fwrite($f2, $str4log."\n");
-	fclose($f2);
-	
-	$nullRet = `$python runcoffee.py > /dev/null &`;
-	
-} else {
-	echo "This is shorter than the minimum valid string length (".$minlength.").";
-	echo "Thus your submission was not added to the papers list.";
-}
-//
-// } else {
-// 	echo "you should not have reached this point!";
-// }
-?>
+    <br>
+    
+    <?php
+        if (isset($_POST["save_volunteer"])){
+            if (strlen($ID)>$minlength and strlen($ID)>$minlength) {
+            	$f = fopen($paperFile_volunteer,"a") or die("can't open file: ".$paperFile_volunteer);
+            	fwrite($f, "\n".trim($ID));
+                fwrite($f, "\n".trim($_POST['volunteer_name']));
+            	fclose($f);
+            	
+                echo '<META HTTP-EQUIV=Refresh CONTENT="10;url=../">';
+                
+            	echo "<p>You were successfully added as a volunteer.</p>";
+            	echo "<p>It may be several minutes before the main page is updated.</p>";
+            	echo "<p>You will be returned to the main page in 10 seconds.</p>";
+            	$str4log = date('D, d M Y H:i:s')."    ".$ID."    ".$_POST['volunteer_name']." (".$ipOfSubmitter.") added as volunteer.";
+            	$f2 = fopen($logFile,"a") or die("can`t open file: ".$logFile);
+            	fwrite($f2, $str4log."\n");
+            	fclose($f2);
+            } else {
+            	echo "<p>The paper ID or your name is shorter than the minimum valid length (".$minlength.").</p>";
+            	echo "<p>Thus your submission was not added to the volunteer list.</p>";
+            }
+        }
+    ?>
+    
+    <p>Return to the <a href='../'>main page</a></p>
